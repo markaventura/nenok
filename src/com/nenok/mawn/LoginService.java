083986b1376a9b72db1a19e0b87a65d79687fd75
Alvin Garcia <alving@sourcepad.com>
2014-03-21 13:34:39 +0800

    user fields on login

--- src/com/nenok/mawn/LoginService.java
+++ src/com/nenok/mawn/LoginService.java
@@ -12,23 +12,29 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
-import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.widget.Toast;
 
 public class LoginService extends AsyncTask<String, Void, Void> {
 	HttpClient httpclient = new DefaultHttpClient();
 	HttpPost httppost = new HttpPost("http://nenok.herokuapp.com/api/session");
+	String username = "";
+	String password = "";
+
+	public LoginService(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
 
 	@Override
 	protected Void doInBackground(String... arg0) {
 		try {
-			Log.v("Background:", "in-progress");
+			Log.v("email", this.username);
+			Log.v("password", this.password);
 		
 			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-			nameValuePairs.add(new BasicNameValuePair("email", "marka@sourcepad.com"));
-		    nameValuePairs.add(new BasicNameValuePair("password", "password"));
+			nameValuePairs.add(new BasicNameValuePair("email", this.username));
+		    nameValuePairs.add(new BasicNameValuePair("password", this.password));
 			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 			HttpResponse response = httpclient.execute(httppost);
 			
--- src/com/nenok/mawn/MainActivity.java
+++ src/com/nenok/mawn/MainActivity.java
@@ -1,61 +1,38 @@
 package com.nenok.mawn;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener{
-//	HttpClient httpclient = new DefaultHttpClient();
-//	HttpPost httppost = new HttpPost("http://nenok.herokuapp.com/api/users");
 	Button buttonStart;
+	EditText usernameFld, passwordFld;
+	String username, password;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Toast.makeText( getApplicationContext(), "Open!", Toast.LENGTH_SHORT).show();
-		buttonStart = (Button) findViewById(R.id.button1);
-		buttonStart.setOnClickListener(this);
+
+		usernameFld = (EditText) findViewById(R.id.uname);
 		
+		
+		passwordFld = (EditText) findViewById(R.id.passw);
+		
+		buttonStart = (Button) findViewById(R.id.login);
+		buttonStart.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
+		username = usernameFld.getText().toString();
+		password = passwordFld.getText().toString();
 
-		new LoginService().execute("http://nenok.herokuapp.com/api/users");
-		
-//		try {
-//			Toast.makeText( getApplicationContext(), "Click!", Toast.LENGTH_SHORT).show();
-//		
-//			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-//			nameValuePairs.add(new BasicNameValuePair("email", "marka@sourcepad.com"));
-//		    nameValuePairs.add(new BasicNameValuePair("password", "passsword"));
-//			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-//			HttpResponse response = httpclient.execute(httppost);
-//			
-//			Toast.makeText( getApplicationContext(), response.toString(), Toast.LENGTH_SHORT).show();
-//		        
-//		} catch (IOException e) {
-//			Toast.makeText( getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
-//		      
-//		} 
+		new LoginService(username, password).execute("http://nenok.herokuapp.com/api/users");
 	}
 
 
